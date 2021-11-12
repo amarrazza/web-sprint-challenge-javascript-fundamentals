490a0fd125c80493c6cb5d5f88e71ebd28022a30
Austin  Marrazza <austinmarrazza@Austins-MacBook-Pro.local>
2021-11-12 10:43:20 -0800

    completed tasks 1 and 2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+// The nested function is able to access the internal variable because it is inheriting the variables used in the parent function.
 
 
 
@@ -28,10 +28,14 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(number) {
+  let counter = 0;
+  for (let i = 0; i <= number; i++){
+    counter += i;
   }
+  return counter;
+}
+console.log(summation(4));
  
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
